6470617a267b73fd285fc3778af692a35a47b7fb
cuongle1025 <cuongle1025@gmail.com>
2024-12-17 14:29:42 -0500

    add router

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,12 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
-const app = express()
 const START_SERVER = () => {
-  app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray())
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  const app = express()
+
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
